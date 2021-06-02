08300a8ac382df3e5e647da1adf4afc640b6c2b3
cyrildav <cyril.david51@hotmail.fr>
2021-06-02 10:05:16 +0200

    setup eslint

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const port = 8000;
-const express = require ('express')
+const express = require('express');
+
 const app = express();
 app.use(express.json());
 
-const requestHandler = ( request, response) => {
-    console.log(request.url);
-    response.end('Hello Node.js Server!');
-}
+app.get('/', (request, response) => {
+  response.send('Welcome to Express');
+});
 
-app.get("/", (request, response) => {
-
-    response.send("Welcome to Express");
-  
-  });
-
-app.listen(port, (err)=> {
-    if (err) {
-        console.error('Something bad happened');
-    } else {
-        console.log(`server is listening on ${port}`);
-    }
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Something bad happened');
+  } else {
+    console.log(`server is listening on ${port}`);
+  }
 });
