c421e3ace6f9f01e6f353e3b1c02e356fbebc21c
cyrildav <cyril.david51@hotmail.fr>
2021-06-04 10:10:58 +0200

    First request

--- sql/pipelette.sql
+++ sql/pipelette.sql
@@ -1,10 +1,11 @@
+DROP DATABASE pipelette;
 CREATE DATABASE pipelette;
 use pipelette;
 CREATE TABLE `user` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `firstname` VARCHAR(100) NOT NULL,
     `lastname` VARCHAR(100) NOT NULL,
-    `created_at` DATETIME NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `mail` VARCHAR(100) NOT NULL,
     `phone` VARCHAR(25) NULL,
     `password` BINARY(64) NOT NULL,
@@ -19,14 +20,8 @@ CREATE TABLE `playlist` (
 
 CREATE TABLE `capsule` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `audio_file` VARCHAR(255) NOT NULL,
-    `audio_url` VARCHAR(255) NOT NULL,
-    `location` VARCHAR(255) NOT NULL
-    `date_record` DATETIME NOT NULL,
+    `audio_path` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `audio_title` VARCHAR(100) NOT NULL,
-    `receiver_firstname` VARCHAR(255) NOT NULL,
-    `receiver_lastname` VARCHAR(255) NOT NULL,
-    `receiver_mail` VARCHAR(100) NOT NULL,
-    `receiver_phone` VARCHAR(25) NOT NULL,
     PRIMARY KEY (`id`)
 );
