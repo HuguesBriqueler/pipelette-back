78e769e194dab115d5c10b1acf20b1fd2a3602a9
denis-webpro <denis.webpro@gmail.com>
2021-06-16 16:44:44 +0200

    Removal of duplicate code with Hugues

--- routes/capsules.js
+++ routes/capsules.js
@@ -2,31 +2,6 @@ const db = require("../db-config");
 
 const capsuleRoutes = require("express").Router();
 
-capsuleRoutes.get("/", (req, res) => {
-  db.query("SELECT * from capsule", (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500);
-    } else {
-      res.status(200).json(results);
-    }
-  });
-});
-
-capsuleRoutes.get("/:id", (req, res) => {
-  db.query(
-    "SELECT * FROM capsule WHERE id = ?",
-    [req.params.id],
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Error retrieving a capsule from database");
-      } else {
-        res.json(results[0]);
-      }
-    }
-  );
-});
-
 capsuleRoutes.post("/", (req, res) => {
   const { audio_path, audio_title } = req.body;
   db.query(
