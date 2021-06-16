b7b03a59e3bfe8b254f16968040a126e9a3a8563
denis-webpro <denis.webpro@gmail.com>
2021-06-16 14:53:21 +0200

    Added routes

--- routes/capsules.js
+++ routes/capsules.js
@@ -1,28 +1,49 @@
 const db = require("../db-config");
 
-const capsuleRoutes = require('express').Router();
+const capsuleRoutes = require("express").Router();
 
 capsuleRoutes.get("/", (req, res) => {
-  res.send("Welcome to the Capsules page")
-})
+  db.query("SELECT * from capsule", (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500);
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
 
-capsuleRoutes.post("/", (req,res) => {
+capsuleRoutes.get("/:id", (req, res) => {
+  db.query(
+    "SELECT * FROM capsule WHERE id = ?",
+    [req.params.id],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error retrieving a capsule from database");
+      } else {
+        res.json(results[0]);
+      }
+    }
+  );
+});
+
+capsuleRoutes.post("/", (req, res) => {
   const { audio_path, audio_title } = req.body;
   db.query(
-    'INSERT INTO capsule(audio_path, audio_title) VALUES (?, ?)',
+    "INSERT INTO capsule(audio_path, audio_title) VALUES (?, ?)",
     [audio_path, audio_title],
     (err, results) => {
       if (err) {
         console.log(err);
-        res.status(500).send('OOPS');
+        res.status(500).send("OOPS");
       } else {
         db.query(
-          'SELECT * FROM capsule WHERE id = ?',
+          "SELECT * FROM capsule WHERE id = ?",
           [results.insertId],
           (err, results) => {
             if (err) {
               console.log(err);
-              res.status(500).send('OOPS');
+              res.status(500).send("OOPS");
             } else {
               res.status(201).json(results[0]);
             }
@@ -31,6 +52,18 @@ capsuleRoutes.post("/", (req,res) => {
       }
     }
   );
+});
+
+capsuleRoutes.delete("/:id", (req, res) => {
+  const capsuleId = req.params.id;
+  db.query("DELETE FROM capsule WHERE id = ?", [capsuleId], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error deleting an capsule");
+    } else {
+      res.status(200).send("Capsule deleted!");
+    }
+  });
 });
 
 module.exports = capsuleRoutes;
