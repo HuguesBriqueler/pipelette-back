67bfbd39fe8cb0115fa74e23fda49e9c0deea701
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-06-23 14:03:55 +0200

    Updated database : added foreign keys in tables + created intermediate table playlistCapsule

--- sql/pipelette.sql
+++ sql/pipelette.sql
@@ -13,7 +13,9 @@ CREATE TABLE `user` (
 CREATE TABLE `playlist` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    `UserId` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `FK_PlaylistOwner` FOREIGN KEY (`UserId`) REFERENCES user(id)
 );
 
 CREATE TABLE `capsule` (
@@ -21,5 +23,16 @@ CREATE TABLE `capsule` (
     `audio_path` VARCHAR(255) NOT NULL,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `audio_title` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`id`)
+    `UserId` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT FK_CapsuleOwner FOREIGN KEY (UserId) REFERENCES user(id)
 );
+
+CREATE TABLE playlistCapsule (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    PlaylistId INT NOT NULL,
+    CapsuleId INT NOT NULL,
+    CONSTRAINT FK_PlaylistId FOREIGN KEY (PlaylistId) REFERENCES playlist(id),
+    CONSTRAINT FK_CapsuleId FOREIGN KEY (CapsuleId) REFERENCES capsule(id)
+)
